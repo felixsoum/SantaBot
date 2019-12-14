1bb6cea4bf9bb127e6aee8311c310d1724330e50
Unknown <felix@felixsoum.com>
2019-12-14 01:38:50 -0500

    Add raffle

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SantaBot
@@ -12,6 +13,25 @@ namespace SantaBot
         DiscordSocketClient client;
         SocketTextChannel generalChannel;
         SocketTextChannel testChannel;
+        static Random random = new Random();
+
+        List<string> participants = new List<string>()
+        {
+            "Nanith Omicron",
+            "Alonso",
+            "Nicolas",
+            "Alejandro",
+            "Sarkis",
+            "Samuel",
+            "Minh",
+            "Youcef",
+            "Alexandre",
+            "Théophile",
+            "Philip",
+            "Leo",
+            "Daniel",
+            "Julian"
+        };
 
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
@@ -20,10 +40,20 @@ namespace SantaBot
         {
             client = new DiscordSocketClient();
             client.Log += Log;
+            client.MessageReceived += MessageReceived;
             client.Ready += Ready;
             await client.LoginAsync(TokenType.Bot, Secret.Token);
             await client.StartAsync();
             await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!raffle")
+            {
+                string winner = participants[random.Next(participants.Count)];
+                await message.Channel.SendMessageAsync($"The Santa raffle winner is: {winner} :gift:");
+            }
         }
 
         private Task Log(LogMessage msg)
@@ -36,7 +66,7 @@ namespace SantaBot
         {
             generalChannel = (SocketTextChannel)client.GetChannel(GeneralId);
             testChannel = (SocketTextChannel)client.GetChannel(TestId);
-            await testChannel.SendMessageAsync($"All systems operational (v0.01 - Hello World!).");
+            await testChannel.SendMessageAsync($"All systems operational (v0.03 - Raffle Fixed).");
         }
     }
 }
