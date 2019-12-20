243b4c15eac3a4e057e6f37b2c6e4157a6e701ff
Unknown <felix@felixsoum.com>
2019-12-19 23:50:01 -0500

    Raffle 1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace SantaBot
     {
         const ulong GeneralId = 516765447975206915;
         const ulong TestId = 545786319050113066;
+        private const long Me = 139024035894788096;
         DiscordSocketClient client;
         SocketTextChannel generalChannel;
         SocketTextChannel testChannel;
@@ -55,10 +56,28 @@ namespace SantaBot
                 string name = $"Your username is {message.Author.Username}";
                 await message.Channel.SendMessageAsync(name);
             }
-            else if (message.Content == "!raffle" && message.Author.Username.Contains("felix"))
+            else if (message.Content == "!id")
             {
-                string winner = participants[random.Next(participants.Count)];
-                await message.Channel.SendMessageAsync($"The Santa raffle winner is: {winner} :gift:");
+                string name = $"Your id is {message.Author.Id}";
+                await message.Channel.SendMessageAsync(name);
+            }
+            else if (message.Content == "!raffle" && message.Author.Id == Me)
+            {
+                if (message.Author.Id == Me)
+                {
+                    string winner = participants[random.Next(participants.Count)];
+                    await message.Channel.SendMessageAsync($"The Santa raffle #1 winner is: {winner} :gift: Prize = Cuphead");
+                }
+                else
+                {
+                    await message.Channel.SendMessageAsync($"User {message.Author.Username} is not authorized to use !raffle.");
+                }
+            }
+            else if (message.Content == "!who")
+            {
+                string who = String.Join(", ", participants);
+                who = "Participants eligible for the raffle are:\n" + who;
+                await message.Channel.SendMessageAsync(who);
             }
         }
 
@@ -72,7 +91,7 @@ namespace SantaBot
         {
             generalChannel = (SocketTextChannel)client.GetChannel(GeneralId);
             testChannel = (SocketTextChannel)client.GetChannel(TestId);
-            await testChannel.SendMessageAsync($"All systems operational (v0.03 - Raffle Fixed).");
+            await testChannel.SendMessageAsync($"All systems operational (v0.07 - Raffle #1).");
         }
     }
 }
